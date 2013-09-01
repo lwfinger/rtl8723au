4ccdaa004fca58a5ed98b9ccb95f54038bf7cdf0
Larry Finger <Larry.Finger@lwfinger.net>
2013-09-01 09:24:02 -0500

    rtl8723au: Fix smatch warnings in os_dep/recv_linux.c
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- os_dep/recv_linux.c
+++ os_dep/recv_linux.c
@@ -407,14 +407,10 @@ _func_exit_;
 
 _recv_indicatepkt_drop:
 
-	 //enqueue back to free_recv_queue
-	 if(precv_frame)
-		 rtw_free_recvframe(precv_frame, pfree_recv_queue);
-
-	 return _FAIL;
+	 rtw_free_recvframe(precv_frame, pfree_recv_queue);
 
 _func_exit_;
-
+	 return _FAIL;
 }
 
 void rtw_os_read_port(_adapter *padapter, struct recv_buf *precvbuf)
