dd0c87304fd2436a71ecd0bfacde197bad7fbe37
Larry Finger <Larry.Finger@lwfinger.net>
2013-09-11 17:59:00 -0500

    rtl8723au: Fix smatch warning in core/rtw_debug.c
    Smatch lists the following:
      CHECK   /home/finger/rtl8723au/core/rtw_debug.c
    /home/finger/rtl8723au/core/rtw_debug.c:688 proc_set_rx_signal() warn: unsigned 'signal_strength' is never less than zero.
    
    The offending statement is testing an unsigned variable.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- core/rtw_debug.c
+++ core/rtw_debug.c
@@ -684,8 +684,7 @@ int proc_set_rx_signal(struct file *file, const char __user *buffer,
 		if(is_signal_dbg && num!=2)
 			return count;
 
-		signal_strength = signal_strength>100?100:signal_strength;
-		signal_strength = signal_strength<0?0:signal_strength;
+		signal_strength = signal_strength > 100 ? 100 : signal_strength;
 
 		padapter->recvpriv.is_signal_dbg = is_signal_dbg;
 		padapter->recvpriv.signal_strength_dbg=signal_strength;
