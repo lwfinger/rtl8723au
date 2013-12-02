162f5e402de7415b56c2049cccb7dd1a49bc8e8e
Larry Finger <Larry.Finger@lwfinger.net>
2013-12-01 22:17:58 -0600

    rtl8723au: Remove C99 comments from hal/hal_com.c
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- hal/hal_com.c
+++ hal/hal_com.c
@@ -65,15 +65,14 @@ void dump_chip_info(HAL_VERSION	ChipVersion)
 	DBG_8723A("%s", buf);
 }
 
-
 #define	EEPROM_CHANNEL_PLAN_BY_HW_MASK	0x80
 
-u8	//return the final channel plan decision
+u8	/* return the final channel plan decision */
 hal_com_get_channel_plan(
 	IN	PADAPTER	padapter,
-	IN	u8			hw_channel_plan,	//channel plan from HW (efuse/eeprom)
-	IN	u8			sw_channel_plan,	//channel plan from SW (registry/module param)
-	IN	u8			def_channel_plan,	//channel plan used when the former two is invalid
+	IN	u8			hw_channel_plan,	/* channel plan from HW (efuse/eeprom) */
+	IN	u8			sw_channel_plan,	/* channel plan from SW (registry/module param) */
+	IN	u8			def_channel_plan,	/* channel plan used when the former two is invalid */
 	IN	bool		AutoLoadFail
 	)
 {
@@ -106,7 +105,7 @@ u8	MRateToHwRate(u8 rate)
 
 	switch(rate)
 	{
-		// CCK and OFDM non-HT rates
+		/*  CCK and OFDM non-HT rates */
 	case IEEE80211_CCK_RATE_1MB:	ret = DESC_RATE1M;	break;
 	case IEEE80211_CCK_RATE_2MB:	ret = DESC_RATE2M;	break;
 	case IEEE80211_CCK_RATE_5MB:	ret = DESC_RATE5_5M;	break;
@@ -120,15 +119,15 @@ u8	MRateToHwRate(u8 rate)
 	case IEEE80211_OFDM_RATE_48MB:	ret = DESC_RATE48M;	break;
 	case IEEE80211_OFDM_RATE_54MB:	ret = DESC_RATE54M;	break;
 
-		// HT rates since here
-	//case MGN_MCS0:		ret = DESC_RATEMCS0;	break;
-	//case MGN_MCS1:		ret = DESC_RATEMCS1;	break;
-	//case MGN_MCS2:		ret = DESC_RATEMCS2;	break;
-	//case MGN_MCS3:		ret = DESC_RATEMCS3;	break;
-	//case MGN_MCS4:		ret = DESC_RATEMCS4;	break;
-	//case MGN_MCS5:		ret = DESC_RATEMCS5;	break;
-	//case MGN_MCS6:		ret = DESC_RATEMCS6;	break;
-	//case MGN_MCS7:		ret = DESC_RATEMCS7;	break;
+		/*  HT rates since here */
+	/* case MGN_MCS0:		ret = DESC_RATEMCS0;	break; */
+	/* case MGN_MCS1:		ret = DESC_RATEMCS1;	break; */
+	/* case MGN_MCS2:		ret = DESC_RATEMCS2;	break; */
+	/* case MGN_MCS3:		ret = DESC_RATEMCS3;	break; */
+	/* case MGN_MCS4:		ret = DESC_RATEMCS4;	break; */
+	/* case MGN_MCS5:		ret = DESC_RATEMCS5;	break; */
+	/* case MGN_MCS6:		ret = DESC_RATEMCS6;	break; */
+	/* case MGN_MCS7:		ret = DESC_RATEMCS7;	break; */
 
 	default:		break;
 	}
@@ -143,7 +142,7 @@ void	HalSetBrateCfg(
 {
 	u8	i, is_brate, brate;
 
-	for(i=0;i<NDIS_802_11_LENGTH_RATES_EX;i++)
+	for(i = 0;i<NDIS_802_11_LENGTH_RATES_EX;i++)
 	{
 		is_brate = mBratesOS[i] & IEEE80211_BASIC_RATE_MASK;
 		brate = mBratesOS[i] & 0x7f;
@@ -176,15 +175,15 @@ _OneOutPipeMapping(
 {
 	struct dvobj_priv	*pdvobjpriv = adapter_to_dvobj(pAdapter);
 
-	pdvobjpriv->Queue2Pipe[0] = pdvobjpriv->RtOutPipe[0];//VO
-	pdvobjpriv->Queue2Pipe[1] = pdvobjpriv->RtOutPipe[0];//VI
-	pdvobjpriv->Queue2Pipe[2] = pdvobjpriv->RtOutPipe[0];//BE
-	pdvobjpriv->Queue2Pipe[3] = pdvobjpriv->RtOutPipe[0];//BK
+	pdvobjpriv->Queue2Pipe[0] = pdvobjpriv->RtOutPipe[0];/* VO */
+	pdvobjpriv->Queue2Pipe[1] = pdvobjpriv->RtOutPipe[0];/* VI */
+	pdvobjpriv->Queue2Pipe[2] = pdvobjpriv->RtOutPipe[0];/* BE */
+	pdvobjpriv->Queue2Pipe[3] = pdvobjpriv->RtOutPipe[0];/* BK */
 
-	pdvobjpriv->Queue2Pipe[4] = pdvobjpriv->RtOutPipe[0];//BCN
-	pdvobjpriv->Queue2Pipe[5] = pdvobjpriv->RtOutPipe[0];//MGT
-	pdvobjpriv->Queue2Pipe[6] = pdvobjpriv->RtOutPipe[0];//HIGH
-	pdvobjpriv->Queue2Pipe[7] = pdvobjpriv->RtOutPipe[0];//TXCMD
+	pdvobjpriv->Queue2Pipe[4] = pdvobjpriv->RtOutPipe[0];/* BCN */
+	pdvobjpriv->Queue2Pipe[5] = pdvobjpriv->RtOutPipe[0];/* MGT */
+	pdvobjpriv->Queue2Pipe[6] = pdvobjpriv->RtOutPipe[0];/* HIGH */
+	pdvobjpriv->Queue2Pipe[7] = pdvobjpriv->RtOutPipe[0];/* TXCMD */
 }
 
 static VOID
@@ -195,42 +194,40 @@ _TwoOutPipeMapping(
 {
 	struct dvobj_priv	*pdvobjpriv = adapter_to_dvobj(pAdapter);
 
-	if(bWIFICfg){ //WMM
+	if(bWIFICfg){ /* WMM */
 
-		//	BK,	BE,	VI,	VO,	BCN,	CMD,MGT,HIGH,HCCA
-		//{  0,		1,	0,	1,	0,	0,	0,	0,		0	};
-		//0:H, 1:L
+		/* 	BK,	BE,	VI,	VO,	BCN,	CMD, MGT, HIGH, HCCA */
+		/*   0,		1,	0,	1,	0,	0,	0,	0,		0	}; */
+		/* 0:H, 1:L */
 
-		pdvobjpriv->Queue2Pipe[0] = pdvobjpriv->RtOutPipe[1];//VO
-		pdvobjpriv->Queue2Pipe[1] = pdvobjpriv->RtOutPipe[0];//VI
-		pdvobjpriv->Queue2Pipe[2] = pdvobjpriv->RtOutPipe[1];//BE
-		pdvobjpriv->Queue2Pipe[3] = pdvobjpriv->RtOutPipe[0];//BK
+		pdvobjpriv->Queue2Pipe[0] = pdvobjpriv->RtOutPipe[1];/* VO */
+		pdvobjpriv->Queue2Pipe[1] = pdvobjpriv->RtOutPipe[0];/* VI */
+		pdvobjpriv->Queue2Pipe[2] = pdvobjpriv->RtOutPipe[1];/* BE */
+		pdvobjpriv->Queue2Pipe[3] = pdvobjpriv->RtOutPipe[0];/* BK */
 
-		pdvobjpriv->Queue2Pipe[4] = pdvobjpriv->RtOutPipe[0];//BCN
-		pdvobjpriv->Queue2Pipe[5] = pdvobjpriv->RtOutPipe[0];//MGT
-		pdvobjpriv->Queue2Pipe[6] = pdvobjpriv->RtOutPipe[0];//HIGH
-		pdvobjpriv->Queue2Pipe[7] = pdvobjpriv->RtOutPipe[0];//TXCMD
-
-	}
-	else{//typical setting
-
-
-		//BK,	BE,	VI,	VO,	BCN,	CMD,MGT,HIGH,HCCA
-		//{  1,		1,	0,	0,	0,	0,	0,	0,		0	};
-		//0:H, 1:L
-
-		pdvobjpriv->Queue2Pipe[0] = pdvobjpriv->RtOutPipe[0];//VO
-		pdvobjpriv->Queue2Pipe[1] = pdvobjpriv->RtOutPipe[0];//VI
-		pdvobjpriv->Queue2Pipe[2] = pdvobjpriv->RtOutPipe[1];//BE
-		pdvobjpriv->Queue2Pipe[3] = pdvobjpriv->RtOutPipe[1];//BK
-
-		pdvobjpriv->Queue2Pipe[4] = pdvobjpriv->RtOutPipe[0];//BCN
-		pdvobjpriv->Queue2Pipe[5] = pdvobjpriv->RtOutPipe[0];//MGT
-		pdvobjpriv->Queue2Pipe[6] = pdvobjpriv->RtOutPipe[0];//HIGH
-		pdvobjpriv->Queue2Pipe[7] = pdvobjpriv->RtOutPipe[0];//TXCMD
+		pdvobjpriv->Queue2Pipe[4] = pdvobjpriv->RtOutPipe[0];/* BCN */
+		pdvobjpriv->Queue2Pipe[5] = pdvobjpriv->RtOutPipe[0];/* MGT */
+		pdvobjpriv->Queue2Pipe[6] = pdvobjpriv->RtOutPipe[0];/* HIGH */
+		pdvobjpriv->Queue2Pipe[7] = pdvobjpriv->RtOutPipe[0];/* TXCMD */
 
 	}
+	else{/* typical setting */
 
+		/* BK,	BE,	VI,	VO,	BCN,	CMD, MGT, HIGH, HCCA */
+		/*   1,		1,	0,	0,	0,	0,	0,	0,		0	}; */
+		/* 0:H, 1:L */
+
+		pdvobjpriv->Queue2Pipe[0] = pdvobjpriv->RtOutPipe[0];/* VO */
+		pdvobjpriv->Queue2Pipe[1] = pdvobjpriv->RtOutPipe[0];/* VI */
+		pdvobjpriv->Queue2Pipe[2] = pdvobjpriv->RtOutPipe[1];/* BE */
+		pdvobjpriv->Queue2Pipe[3] = pdvobjpriv->RtOutPipe[1];/* BK */
+
+		pdvobjpriv->Queue2Pipe[4] = pdvobjpriv->RtOutPipe[0];/* BCN */
+		pdvobjpriv->Queue2Pipe[5] = pdvobjpriv->RtOutPipe[0];/* MGT */
+		pdvobjpriv->Queue2Pipe[6] = pdvobjpriv->RtOutPipe[0];/* HIGH */
+		pdvobjpriv->Queue2Pipe[7] = pdvobjpriv->RtOutPipe[0];/* TXCMD */
+
+	}
 }
 
 static VOID _ThreeOutPipeMapping(
@@ -240,41 +237,38 @@ static VOID _ThreeOutPipeMapping(
 {
 	struct dvobj_priv	*pdvobjpriv = adapter_to_dvobj(pAdapter);
 
-	if(bWIFICfg){//for WMM
+	if(bWIFICfg){/* for WMM */
 
-		//	BK,	BE,	VI,	VO,	BCN,	CMD,MGT,HIGH,HCCA
-		//{  1,		2,	1,	0,	0,	0,	0,	0,		0	};
-		//0:H, 1:N, 2:L
+		/* 	BK,	BE,	VI,	VO,	BCN,	CMD, MGT, HIGH, HCCA */
+		/*   1,		2,	1,	0,	0,	0,	0,	0,		0	}; */
+		/* 0:H, 1:N, 2:L */
 
-		pdvobjpriv->Queue2Pipe[0] = pdvobjpriv->RtOutPipe[0];//VO
-		pdvobjpriv->Queue2Pipe[1] = pdvobjpriv->RtOutPipe[1];//VI
-		pdvobjpriv->Queue2Pipe[2] = pdvobjpriv->RtOutPipe[2];//BE
-		pdvobjpriv->Queue2Pipe[3] = pdvobjpriv->RtOutPipe[1];//BK
+		pdvobjpriv->Queue2Pipe[0] = pdvobjpriv->RtOutPipe[0];/* VO */
+		pdvobjpriv->Queue2Pipe[1] = pdvobjpriv->RtOutPipe[1];/* VI */
+		pdvobjpriv->Queue2Pipe[2] = pdvobjpriv->RtOutPipe[2];/* BE */
+		pdvobjpriv->Queue2Pipe[3] = pdvobjpriv->RtOutPipe[1];/* BK */
 
-		pdvobjpriv->Queue2Pipe[4] = pdvobjpriv->RtOutPipe[0];//BCN
-		pdvobjpriv->Queue2Pipe[5] = pdvobjpriv->RtOutPipe[0];//MGT
-		pdvobjpriv->Queue2Pipe[6] = pdvobjpriv->RtOutPipe[0];//HIGH
-		pdvobjpriv->Queue2Pipe[7] = pdvobjpriv->RtOutPipe[0];//TXCMD
+		pdvobjpriv->Queue2Pipe[4] = pdvobjpriv->RtOutPipe[0];/* BCN */
+		pdvobjpriv->Queue2Pipe[5] = pdvobjpriv->RtOutPipe[0];/* MGT */
+		pdvobjpriv->Queue2Pipe[6] = pdvobjpriv->RtOutPipe[0];/* HIGH */
+		pdvobjpriv->Queue2Pipe[7] = pdvobjpriv->RtOutPipe[0];/* TXCMD */
 
+	} else{/* typical setting */
+
+		/* 	BK,	BE,	VI,	VO,	BCN,	CMD, MGT, HIGH, HCCA */
+		/*   2,		2,	1,	0,	0,	0,	0,	0,		0	}; */
+		/* 0:H, 1:N, 2:L */
+
+		pdvobjpriv->Queue2Pipe[0] = pdvobjpriv->RtOutPipe[0];/* VO */
+		pdvobjpriv->Queue2Pipe[1] = pdvobjpriv->RtOutPipe[1];/* VI */
+		pdvobjpriv->Queue2Pipe[2] = pdvobjpriv->RtOutPipe[2];/* BE */
+		pdvobjpriv->Queue2Pipe[3] = pdvobjpriv->RtOutPipe[2];/* BK */
+
+		pdvobjpriv->Queue2Pipe[4] = pdvobjpriv->RtOutPipe[0];/* BCN */
+		pdvobjpriv->Queue2Pipe[5] = pdvobjpriv->RtOutPipe[0];/* MGT */
+		pdvobjpriv->Queue2Pipe[6] = pdvobjpriv->RtOutPipe[0];/* HIGH */
+		pdvobjpriv->Queue2Pipe[7] = pdvobjpriv->RtOutPipe[0];/* TXCMD */
 	}
-	else{//typical setting
-
-
-		//	BK,	BE,	VI,	VO,	BCN,	CMD,MGT,HIGH,HCCA
-		//{  2,		2,	1,	0,	0,	0,	0,	0,		0	};
-		//0:H, 1:N, 2:L
-
-		pdvobjpriv->Queue2Pipe[0] = pdvobjpriv->RtOutPipe[0];//VO
-		pdvobjpriv->Queue2Pipe[1] = pdvobjpriv->RtOutPipe[1];//VI
-		pdvobjpriv->Queue2Pipe[2] = pdvobjpriv->RtOutPipe[2];//BE
-		pdvobjpriv->Queue2Pipe[3] = pdvobjpriv->RtOutPipe[2];//BK
-
-		pdvobjpriv->Queue2Pipe[4] = pdvobjpriv->RtOutPipe[0];//BCN
-		pdvobjpriv->Queue2Pipe[5] = pdvobjpriv->RtOutPipe[0];//MGT
-		pdvobjpriv->Queue2Pipe[6] = pdvobjpriv->RtOutPipe[0];//HIGH
-		pdvobjpriv->Queue2Pipe[7] = pdvobjpriv->RtOutPipe[0];//TXCMD
-	}
-
 }
 
 bool
@@ -306,7 +300,6 @@ Hal_MappingOutPipe(
 	}
 
 	return result;
-
 }
 
 void hal_init_macaddr(_adapter *adapter)
