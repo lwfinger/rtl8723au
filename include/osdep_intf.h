182d3a3ea57b3d7002c63778a1764429deeb1d17
Larry Finger <Larry.Finger@lwfinger.net>
2013-12-09 19:28:18 -0600

    rtl8723au: Remove dependence on KERNEL_VERSION for include/osdep_intf.h
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- include/osdep_intf.h
+++ include/osdep_intf.h
@@ -84,9 +84,7 @@ int rtw_ioctl(struct net_device *dev, struct ifreq *rq, int cmd);
 int rtw_init_netdev_name(struct net_device *pnetdev, const char *ifname);
 struct net_device *rtw_init_netdev(struct rtw_adapter *padapter);
 
-#if (LINUX_VERSION_CODE>=KERNEL_VERSION(2,6,35))
 u16 rtw_recv_select_queue(struct sk_buff *skb);
-#endif //LINUX_VERSION_CODE>=KERNEL_VERSION(2,6,35)
 
 #ifdef CONFIG_PROC_DEBUG
 void rtw_proc_init_one(struct net_device *dev);
