86d17e9002a265e94f773ce9ca4cb3edb71e8c12
Larry Finger <Larry.Finger@lwfinger.net>
2014-01-08 13:58:51 -0600

    rtl8723au: Remove dead code around CONFIG_UPDATE_INDICATE_SEQ_WHILE_PROCESS_ADDBA_REQ
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- core/rtw_wlan_util.c
+++ core/rtw_wlan_util.c
@@ -2046,15 +2046,7 @@ void process_addba_req(struct rtw_adapter *padapter, u8 *paddba_req, u8 *addr)
 
 		preorder_ctrl = &psta->recvreorder_ctrl[tid];
 
-		#ifdef CONFIG_UPDATE_INDICATE_SEQ_WHILE_PROCESS_ADDBA_REQ
-		preorder_ctrl->indicate_seq = start_seq;
-		#ifdef DBG_RX_SEQ
-		DBG_8723A("DBG_RX_SEQ %s:%d IndicateSeq: %d, start_seq: %d\n", __FUNCTION__, __LINE__,
-			preorder_ctrl->indicate_seq, start_seq);
-		#endif
-		#else
 		preorder_ctrl->indicate_seq = 0xffff;
-		#endif
 
 		preorder_ctrl->enable =(pmlmeinfo->bAcceptAddbaReq == true)? true :false;
 	}
