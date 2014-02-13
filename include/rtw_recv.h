ece3e201a6f4ddb1cdb77449977eeefa52d33445
Jes Sorensen <Jes.Sorensen@redhat.com>
2014-02-13 21:09:11 +0100

    Remove unused get_rxbuf_desc()
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>

--- include/rtw_recv.h
+++ include/rtw_recv.h
@@ -491,18 +491,6 @@ static inline u8 *recvframe_pull_tail(struct recv_frame *precvframe, int sz)
 }
 
 
-
-static inline unsigned char *get_rxbuf_desc(struct recv_frame *precvframe)
-{
-	unsigned char *buf_desc;
-
-	if(precvframe==NULL)
-		return NULL;
-
-	return buf_desc;
-}
-
-
 static inline int get_recvframe_len(struct recv_frame *precvframe)
 {
 	return precvframe->len;
