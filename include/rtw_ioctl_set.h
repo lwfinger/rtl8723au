832a1c3bd9fcba69fb5ec76a5d17e561533c22be
Larry Finger <Larry.Finger@lwfinger.net>
2014-03-20 11:22:58 -0500

    Remove typedef statements from include/rtw_ioctl_set.h
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- include/rtw_ioctl_set.h
+++ include/rtw_ioctl_set.h
@@ -18,13 +18,10 @@
 #include <drv_types.h>
 
 
-typedef u8 NDIS_802_11_PMKID_VALUE[16];
-
-typedef struct _BSSIDInfo {
+struct bssid_info {
 	unsigned char  BSSID[6];
-	NDIS_802_11_PMKID_VALUE  PMKID;
-} BSSIDInfo, *PBSSIDInfo;
-
+	u8  PMKID[16];
+};
 
 u8 rtw_set_802_11_add_key(struct rtw_adapter *padapter,
 			  struct ndis_802_11_key *key);
