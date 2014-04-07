dd7355b50cc58ea5d8aa7cce57c4948ce7cca7b5
Larry Finger <Larry.Finger@lwfinger.net>
2014-04-06 22:58:07 -0500

    rtl8723au: Fix builds for kernel 3.13
    This is a temporary fix. If you want to run this branch, you
    really should be running kernel 3.14!!!!
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- include/ethernet.h
+++ include/ethernet.h
@@ -17,17 +17,25 @@
 #ifndef __INC_ETHERNET_H
 #define __INC_ETHERNET_H
 
+#include <linux/module.h>
+
 #define LLC_HEADER_SIZE			6	/*  LLC Header Length */
 
-#ifndef ether_addr_copy
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(3, 14, 0))
 static inline void ether_addr_copy(u8 *dst, const u8 *src)
 {
-        u16 *a = (u16 *)dst;
-        const u16 *b = (const u16 *)src;
+#if defined(CONFIG_HAVE_EFFICIENT_UNALIGNED_ACCESS)
+	*(u32 *)dst = *(const u32 *)src;
+	*(u16 *)(dst + 4) = *(const u16 *)(src + 4);
+#else
+	u16 *a = (u16 *)dst;
+	const u16 *b = (const u16 *)src;
 
-        a[0] = b[0];
-        a[1] = b[1];
-        a[2] = b[2];
+	a[0] = b[0];
+	a[1] = b[1];
+	a[2] = b[2];
 #endif
 }
+#endif
+
 #endif /*  #ifndef __INC_ETHERNET_H */
